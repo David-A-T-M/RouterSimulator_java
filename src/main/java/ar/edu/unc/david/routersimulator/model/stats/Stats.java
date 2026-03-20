fc31728dd12a62bd243e269f8034042faefd5b74
David Trujillo Medina (Fedora) <d.trujillo@unc.edu.ar>
2026-03-20 16:52:14 -0300

    refactor: remove unused imports in Stats and NetworkNode classes
    - Clean up unused imports from Router and Terminal-related classes.

--- src/main/java/ar/edu/unc/david/routersimulator/model/stats/Stats.java
+++ src/main/java/ar/edu/unc/david/routersimulator/model/stats/Stats.java
@@ -1,8 +1,6 @@
 package ar.edu.unc.david.routersimulator.model.stats;
 
 import ar.edu.unc.david.routersimulator.model.IpAddress;
-import ar.edu.unc.david.routersimulator.model.nodes.Router;
-import ar.edu.unc.david.routersimulator.model.nodes.Terminal;
 
 /**
  * A common interface for stats snapshots from both {@link Router}s and {@link Terminal}s, to be
